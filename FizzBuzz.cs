e866790051636dd457753852a50abde02f1abcbe
GiIIy <74792652+GiIIy@users.noreply.github.com>
2021-06-07 23:20:07 +0100

    Update FizzBuzz.cs

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -10,15 +10,17 @@ class MainClass {
   public static void fizzBuzz(){                  // Creates a function called 'fizzBuzz'      
     
     for (int i = 1; i<101; i++){                  // Runs a for loop starting from one up until 100
-      if(i % 3 == 0 && i % 5 == 0){               // Checks to see if i is divisible by 3 and 5 (without remainders) if so will output 'FizzBuzz'
-        Console.WriteLine("FizzBuzz");
-      }else if(i % 3 == 0){                       // Checks to see if i is divisible by 3 (without remainders) if so will output 'Fizz'   
-        Console.WriteLine("Fizz");
-      }else if(i % 5 == 0){                       // Checks to see if i is divisible by 5 (without remainders) if so will output 'Buzz'  
-        Console.WriteLine("Buzz");
-      }else{                                      // If none of these conditions are true then it will just print out the number
-        Console.WriteLine(i);
+      string f= "";                               // Creates Variable f
+      if(i % 3 == 0){                             // Checks to see if i is divisible by 3 (without remainders) if so add 'fizz' to f
+        f = "Fizz";
+      }if(i % 5 == 0){                            // Checks to see if i is divisible by 5 (without remainders) if so add 'buzz' to f  
+        f = f + "Buzz";
+      }if(f==""){                                 // If none of these conditions are true then it will just print out the number
+        Console.Write(i);
       }
+      Console.Write(f);                           // outputs variable f
+      Console.WriteLine();                        // moves to new line
+
     }
   }
 }
