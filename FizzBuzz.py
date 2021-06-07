e5a10c372c34804e73216eff6434bc3a77f26bbe
GiIIy <74792652+GiIIy@users.noreply.github.com>
2021-06-07 23:49:51 +0100

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,16 +1,21 @@
 def fizzBuzz():                                             # Creates a function called 'fizzBuzz'
     
-    for x in range(1,101):                                  # Runs a for loop starting from one up until 100
-        if x % 3 == 0 and x % 5 == 0:                       # Checks to see if i is divisible by 3 and 5 (without remainders) if so will output 'FizzBuzz'
-            print("FizzBuzz")
-        elif x % 3 == 0:                                    # Checks to see if i is divisible by 3 (without remainders) if so will output 'Fizz'
-            print("Fizz")
-        elif x % 5 == 0:                                    # Checks to see if i is divisible by 5 (without remainders) if so will output 'Buzz'
-            print("Buzz")
-        else:                                               # If none of these conditions are true then it will just print out the number
-            print(x)                        
+    for i in range(1,101):                                  # Runs a for loop starting from one up until 100
+        f = ""
+        
+        if x % 3 == 0:                                    # Checks to see if i is divisible by 3 (without remainders) if so will output 'Fizz'
+            f = "Fizz"
+        if x % 5 == 0:                                    # Checks to see if i is divisible by 5 (without remainders) if so will output 'Buzz'
+            f = f + "Buzz"
+        if f=="":                                               # If none of these conditions are true then it will just print out the number
+            f = x   
+            
+        print(f)
 
-
+        
+        
+        
+        
 fizzBuzz()                                                      # Calls the fuction 'fizzBuzz'
 
 
