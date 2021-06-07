5710a3abd6364a62cefb8d7c9075c0e977cb04d5
GiIIy <74792652+GiIIy@users.noreply.github.com>
2021-06-07 23:46:02 +0100

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -7,15 +7,15 @@ public class FizzBuzz {
     public static void fizzBuzz(){                                  // Creates a function called 'fizzBuzz'
 
         for(int i = 1; i<101; i++ ){                                // Runs a for loop starting from one up until 100
-            if(i % 3 == 0 && i % 5 == 0){                           // Checks to see if i is divisible by 3 and 5 (without remainders) if so will output 'FizzBuzz'
-                System.out.println("FizzBuzz");                     
-            }else if(i % 3 == 0){                                   // Checks to see if i is divisible by 3 (without remainders) if so will output 'Fizz'    
-                System.out.println("Fizz"); 
-            }else if(i % 5 == 0){                                   // Checks to see if i is divisible by 5 (without remainders) if so will output 'Buzz'  
-                System.out.println("Buzz");                         
-            }else{                                                  // If none of these conditions are true then it will just print out the number
-                System.out.println(i);                              
+            String f= "";                                           // Creates Variable f
+            if(i % 3 == 0){                                         // Checks to see if i is divisible by 3 (without remainders) if so add 'fizz' to f
+                f = "Fizz";                     
+            }if(i % 3 == 0){                                        // Checks to see if i is divisible by 5 (without remainders) if so add 'buzz' to f  
+                f = f + "Buzz";
+            } if(f==""){                                           // If none of these conditions are true then it will just print out the number
+                f = Integer.toString(i);                 
             }
+            System.out.println(f);                                // outputs variable f
         }
     }
 }
